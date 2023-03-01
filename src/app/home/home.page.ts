6a7eb689a4c7f334cf8c2fba822cb11588c12814
AlexandreSJ <alexandre.sj22@gmail.com>
2023-03-01 01:10:18 -0300

    New home and css adjustments

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { waitForAsync } from '@angular/core/testing';
+import { ToastController } from '@ionic/angular';
 
 @Component({
   selector: 'app-home',
@@ -6,10 +8,45 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.page.scss'],
 })
 export class HomePage implements OnInit {
+  age: string;
+  constructor(
+    private toastController: ToastController,
+    ) { }
 
-  constructor() { }
-
-  ngOnInit() {
+  async presentToast() {
+    waitForAsync
+    const toast = await this.toastController.create({
+      message: 'Selecione as abas para explorar',
+      position: 'bottom',
+      cssClass: 'tabTutorialToast',
+      duration: 12000,
+      buttons: [
+        {
+          icon: 'close',
+          role: 'cancel',
+        }
+      ]
+    });
+    await toast.present();
   }
 
+  async tabTutorialStart(){
+    await new Promise(f => setTimeout(f, 2500));
+    this.presentToast();
+  }
+  
+  ngOnInit() {
+    var today = new Date();
+    var birthDate = new Date('2002/09/17');
+    var age = today.getFullYear() - birthDate.getFullYear();
+    var month = today.getMonth() - birthDate.getMonth();
+    if (month < 0 || (month === 0 && today.getDate() < birthDate.getDate())) {
+        age--;
+    }
+    
+    this.age = age.toString();
+
+    this.tabTutorialStart();
+    
+  }
 }
--- src/app/info/info.page.ts
+++ src/app/info/info.page.ts
@@ -7,17 +7,5 @@ import * as moment from 'moment';
   styleUrls: ['info.page.scss']
 })
 export class InfoPage {
-  age: number;
   constructor() { }
-
-  ngOnInit() {
-    var today = new Date();
-    var birthDate = new Date('2002/09/17');
-    var age = today.getFullYear() - birthDate.getFullYear();
-    var month = today.getMonth() - birthDate.getMonth();
-    if (month < 0 || (month === 0 && today.getDate() < birthDate.getDate())) {
-        age--;
-    }
-    this.age = age;
-  }
 }
